65fb9251d64e0ea9b7ca094aca6e7841e51c9a08
Muhammet <muhammeta7@gmail.com>
2016-09-26 18:45:12 -0400

    dreambooks api call not complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,21 +23,53 @@ $("#ISBN-form").on("submit", function(e){
   e.stopPropagation();
   var isbnInput = $('#isbninput').val().trim();
   // Auto pushes up to book shelf
+
   // Using ISBN # retrieve title author summary and possibly reviews
   database.ref().push({
     ISBN : isbnInput
   })
 
-  // Calls to dreambooks API to get NY Times rating
+  // Calls to dreambooks API to get NY Times reviews and ratings
   var dreambooksURL = "http://api/books/reviews.json?q=" + isbnInput + "&key=da5e557ab077cd7d98bef194bedc0e000c1e75af"
   $.ajax({url: dreambooksURL, method: 'GET'}).done(function(reviews){
     var bookReview = reviews.book.critic_reviews[0].review_link;
     var starRating = reviews.book.critic_reviews[0].star_rating;
     var snippet = reviews.book.critic_reviews[0].snippet;
-    console.log()
+    // jQuery for display when book is clicked on
+    $('#').html('<h3>Star Rating: ')
   });
 
   // Conditionals for star Ratings
+  if ( starRating == 0.5 ){
+    
+  }
+  if ( starRating == 1.0 ){
+    
+  }
+  if ( starRating == 1.5 ){
+    
+  }
+  if ( starRating == 2.0 ){
+    
+  }
+  if ( starRating == 2.5 ){
+    
+  }
+  if ( starRating == 3.0 ){
+    
+  }
+  if ( starRating == 3.5 ){
+    
+  }
+  if ( starRating == 4.0 ){
+    
+  }
+  if ( starRating == 4.5 ){
+    
+  }
+  if ( starRating == 5.0 ){
+    
+  }
 
 
 // Takes user Author/Title input from form and adds new book to virtual Bookshelf
@@ -48,7 +80,7 @@ $("#TA-form").on("submit", function(e){
   var authorInput = $('#authorInput').val().trim()
   // Gives person options based on author or title, then click to send up info
 
-  // Calls to dreambooks API to get 5 book reviews
+  // Calls to dreambooks API to get NY Times book reviews and ratings
   var dreambooksURL = "http://api/books/reviews.json?q=" + titleInput + "&key=da5e557ab077cd7d98bef194bedc0e000c1e75af"
   $.ajax({url: dreambooksURL, method: 'GET'}).done(function(reviews){
     var bookReview = reviews.book.critic_reviews[0].review_link;
@@ -59,3 +91,36 @@ $("#TA-form").on("submit", function(e){
 });
 
   // Conditionals for star ratings
+  if ( starRating == 0.0 ){
+
+  }
+  if ( starRating == 0.5 ){
+    
+  }
+  if ( starRating == 1.0 ){
+    
+  }
+  if ( starRating == 1.5 ){
+    
+  }
+  if ( starRating == 2.0 ){
+    
+  }
+  if ( starRating == 2.5 ){
+    
+  }
+  if ( starRating == 3.0 ){
+    
+  }
+  if ( starRating == 3.5 ){
+    
+  }
+  if ( starRating == 4.0 ){
+    
+  }
+  if ( starRating == 4.5 ){
+    
+  }
+  if ( starRating == 5.0 ){
+    
+  }
