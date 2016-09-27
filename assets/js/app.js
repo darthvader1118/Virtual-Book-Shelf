f53310223965a2c4ebead042bebac2a68c89f1db
Muhammet <muhammeta7@gmail.com>
2016-09-26 20:57:38 -0400

    Dreambooks API call working

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,47 +29,21 @@ $("#ISBN-form").on("submit", function(e){
     ISBN : isbnInput
   })
 
-  // Calls to dreambooks API to get NY Times reviews and ratings
-  var dreambooksURL = "http://api/books/reviews.json?q=" + isbnInput + "&key=da5e557ab077cd7d98bef194bedc0e000c1e75af"
-  $.ajax({url: dreambooksURL, method: 'GET'}).done(function(reviews){
-    var bookReview = reviews.book.critic_reviews[0].review_link;
-    var starRating = reviews.book.critic_reviews[0].star_rating;
-    var snippet = reviews.book.critic_reviews[0].snippet;
-    // jQuery for display when book is clicked on
-    $('#').html('<h3>Star Rating: ')
-  });
+  var reviewLink;
+  var starRating;
 
-  // Conditionals for star Ratings
-  if ( starRating == 0.5 ){
-    
-  }
-  if ( starRating == 1.0 ){
-    
-  }
-  if ( starRating == 1.5 ){
-    
-  }
-  if ( starRating == 2.0 ){
-    
-  }
-  if ( starRating == 2.5 ){
-    
-  }
-  if ( starRating == 3.0 ){
-    
-  }
-  if ( starRating == 3.5 ){
-    
-  }
-  if ( starRating == 4.0 ){
-    
-  }
-  if ( starRating == 4.5 ){
-    
-  }
-  if ( starRating == 5.0 ){
-    
-  }
+  // Calls to dreambooks API to get NY Times reviews and ratings
+  var dreambooksURL = "http://idreambooks.com/api/books/reviews.json?q=" + isbnInput + "&key=da5e557ab077cd7d98bef194bedc0e000c1e75af"
+  $.ajax({url: dreambooksURL, method: 'GET'}).done(function(reviews){
+    reviewLink = reviews.book.critic_reviews[0].review_link;
+    starRating = reviews.book.critic_reviews[0].star_rating;
+    // jQuery for display when book is clicked on
+    // $('#display').html('<h3>Star Rating: ' + starRating + '<h3>')
+    // $('#display').html('<h3>Review Link: ' + reviewLink + '<h3>')
+    console.log(reviewLink);
+    console.log(starRating);
+  });
+});
 
 
 // Takes user Author/Title input from form and adds new book to virtual Bookshelf
@@ -78,49 +52,19 @@ $("#TA-form").on("submit", function(e){
   e.stopPropagation();
   var titleInput = $('#titleInput').val().trim()
   var authorInput = $('#authorInput').val().trim()
+  var authorSplit = authorInput.split(" ").join("+");
+  var titleSplit = titleInput.split(" ").join("+");
   // Gives person options based on author or title, then click to send up info
 
   // Calls to dreambooks API to get NY Times book reviews and ratings
-  var dreambooksURL = "http://api/books/reviews.json?q=" + titleInput + "&key=da5e557ab077cd7d98bef194bedc0e000c1e75af"
+  var dreambooksURL = "http://idreambooks.com/api/books/reviews.json?q=" + titleSplit + "&key=da5e557ab077cd7d98bef194bedc0e000c1e75af"
   $.ajax({url: dreambooksURL, method: 'GET'}).done(function(reviews){
-    var bookReview = reviews.book.critic_reviews[0].review_link;
-    var starRating = reviews.book.critic_reviews[0].star_rating;
-    var snippet = reviews.book.critic_reviews[0].snippet;
-    console.log()
+    reviewLink = reviews.book.critic_reviews[0].review_link;
+    starRating = reviews.book.critic_reviews[0].star_rating;
+    console.log(reviewLink);
+    console.log(starRating);
   });
 });
 
-  // Conditionals for star ratings
-  if ( starRating == 0.0 ){
 
-  }
-  if ( starRating == 0.5 ){
-    
-  }
-  if ( starRating == 1.0 ){
-    
-  }
-  if ( starRating == 1.5 ){
-    
-  }
-  if ( starRating == 2.0 ){
-    
-  }
-  if ( starRating == 2.5 ){
-    
-  }
-  if ( starRating == 3.0 ){
-    
-  }
-  if ( starRating == 3.5 ){
-    
-  }
-  if ( starRating == 4.0 ){
-    
-  }
-  if ( starRating == 4.5 ){
-    
-  }
-  if ( starRating == 5.0 ){
-    
-  }
+ 
