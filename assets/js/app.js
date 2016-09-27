46bd9e5bdd01f6a7c2ee45608195ef168426d209
Mario <Mario>
2016-09-26 20:10:11 -0400

    clicking books pushes them onto book shelf

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
 function bookSearch(){
+
   var search = $('#titleInput').val().trim();
   // parseSearch adds the + sign in between search words, might not need it
   var parseSearch = search.split(" ").join("+");
@@ -12,8 +13,15 @@ function bookSearch(){
     data: {param1: 'value1'},
     success: function(data) {
       for (var i = 0; i < data.items.length; i++) {
-      var titles = data.items[i].volumeInfo.title
-      $('#searchResults').append("<h4>" + titles + "</h4>")
+      var images = data.items[i].volumeInfo.imageLinks.smallThumbnail;
+      var titles = data.items[i].volumeInfo.title;
+      var authors = data.items[i].volumeInfo.authors;
+      var description = data.items[i].volumeInfo.description;
+      var price = data.items[i].saleInfo.retailPrice;
+      var bookTitle = $('<div>').addClass('thisBook');
+      bookTitle.attr({'data-images': images}).attr({'data-description': description}).attr({'data-price': price}).attr({'data-title': titles}).attr({'data-author': authors});
+      bookTitle.append("<h4>" + titles + "</h4>" + " <h5>" + authors + "</h5>");
+      $('#searchResults').append(bookTitle);
     }
 
     }
@@ -30,8 +38,28 @@ function bookSearch(){
     console.log("complete");
   });
   
+   $('#titleInput').val(" ");
 
   return false;
+
+
 }
 
+
 $(document).on('click', '#submit-titleAuthor', bookSearch);
+
+// When user selects from Search Results
+$(document).on('click', '.thisBook', function(){
+  $('#searchResults').empty();
+  console.log($(this).data('title'));
+  var cover = $("<img height='200px'>");
+  var img = $(this).data('images');
+
+  cover.attr('src', img).addClass('coverCSS bookInfo');
+  $('.bookshelf-panel').append(cover);
+});
+
+//Clicking books on shelf to grab info
+$(document).on('click', '.bookInfo', function(){
+  // var title2 = $(this).data('titles')
+});
