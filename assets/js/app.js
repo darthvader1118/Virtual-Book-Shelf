b6e6bbac07540611652a65dab4cbff205f532c46
Stefanie Ding <StefanieDing@StefaniDingsMBP.home>
2016-09-30 00:40:28 -0400

    completed firebase storage and display review link and stars

--- assets/js/app.js
+++ assets/js/app.js
@@ -71,37 +71,38 @@ $(document).on('click', '#submit-titleAuthor', bookSearch);
 
 // When user selects from Search Results
 $(document).on('click', '.thisBook', function(){
+  var cover = $(this).data('images');
+  var title = $(this).data('title');
+  var author = $(this).data('author');
+  var year = $(this).data('year');
+  var description = $(this).data('description');
 
-  // Set up empty array for star rating images and other variables
   var reviewLink;
   var starRating;
-  var search2 = titleVars[j];
-  var parseSearch2 = search2.split(" ").join("+");
+  var search = title.split(" ").join("+");
 
-  var dreambooksURL = "http://idreambooks.com/api/books/reviews.json?q=" + parseSearch2 + "&key=da5e557ab077cd7d98bef194bedc0e000c1e75af"
+  var dreambooksURL = "http://idreambooks.com/api/books/reviews.json?q=" + search + "&key=da5e557ab077cd7d98bef194bedc0e000c1e75af"
 
   $.ajax({url: dreambooksURL, type: 'GET'}).done(function(reviews){
     console.log(reviews);
-    reviewLink = reviews.book.critic_reviews ? reviews.book.critic_reviews[0].review_link : 'no reviews';
-    starRating = reviews.book.critic_reviews ? reviews.book.critic_reviews[0].star_rating : '0';
-    console.log(reviewLink);
-    console.log(starRating);
-  });
-
+    reviewLink = (reviews.book.critic_reviews.length == 0) ? 'no reviews' : reviews.book.critic_reviews[0].review_link;
+    starRating = (reviews.book.critic_reviews.length == 0) ?  '0' : reviews.book.critic_reviews[0].star_rating;
+  
   $('#searchResults').empty();
 
  // Creates local "temporary" object for holding book data
   var newBook = {
-    cover: $(this).data('images'),
-    title: $(this).data('title'),
-    author:  $(this).data('author'),
-    year: $(this).data('year'),
-    description: $(this).data('description')
-    // reviewLink: reviewLink,
-    // starRating: starRating
+    cover: cover,
+    title: title,
+    author: author,
+    year: year,
+    description: description,
+    reviewLink: reviewLink,
+    starRating: starRating
   }
-
+  console.log(newBook);
   database.ref().push(newBook);
+  });
 });
 
 // Function that adds book to database and displays books on bookshelf on page load
@@ -118,23 +119,40 @@ database.ref().on("child_added", function(snapshot) {
 //Clicking books on shelf to grab info
 $(document).on('click', '.bookInfo', function(){
   console.log(this);
+  var currentBook;
   var that = this;
   var displayTitle = $(this).data('title');
   var displayAuthor = $(this).data('author');
   var displaySummary = $(this).data('description');
+  var displayLink;
+  var displayStars;
+  var link;
   // var displayLink = $(this).data('reviewLink');
+  // console.log(displayLink);
   // var displayStars = $(this).data('starRating');
-  // var link = "New York Times"
-
-  // if (displayLink == 'no review'){
-    //link = "No review.";
-  //}
+  // var link = "Review Link"
+ 
+  //limits the id grab by the one that matches the title selected
+  database.ref().orderByChild("title").equalTo(displayTitle).limitToFirst(1).on("child_added", function(snapTest) {
+    currentBook = snapTest.key;
+    console.log(currentBook);
+    database.ref().child('currentBook').remove();
+    displayLink = snapTest.val().reviewLink;
+    displayStars = snapTest.val().starRating;
+    console.log(displayLink + " " + displayStars);
+    
+    if (displayLink == 'no reviews'){
+      link = "No review.";
+    } else{
+      link = "Review Link";
+    }
+  });
 
     //Sweet Alert!
     swal({
       title: displayTitle,
-      text: "<h5>" + displayAuthor + "</h5>" + "<p>" + displaySummary + "</p>" /*+ "<a href=" + displayLink + "> + link + </a>"*/,
-      // imageUrl: "../images/Stars-" + displayStars + ".jpg"
+      text: "<h5>" + displayAuthor + "</h5>" + "<p>" + displaySummary + "</p>" + "<a href=" + displayLink + " target='_blank'>" + link + "</a>",
+      imageUrl: "assets/images/Stars-" + displayStars + ".jpg",
       html: true,
       showCancelButton: true,
       confirmButtonColor: "#DD6B55",
@@ -146,6 +164,9 @@ $(document).on('click', '.bookInfo', function(){
         function(isConfirm){
           if (isConfirm) {
             that.remove();
+            database.ref().on("child_added", function(snapshot) {
+              database.ref().child(currentBook).remove();
+            });
             swal("Removed!", "Your book has been removed from the shelf", "success");
           }
     });
